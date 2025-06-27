e6bb687b8a9271ba3d2cac5ed8e99cb9f0aebc3d
Thanh Tùng Nguyễn <20222052@eaut.edu.vn>
2025-06-27 16:23:44 +0700

    test git

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Router } from 'react-router-dom';
 import HomePage from '~/pages/HomePage';
 
 const App = () => {
     return (
         <div>
-            <HomePage />
+            <Router>
+                <Routes>
+                    <Route path="/home" element={<HomePage />} />
+                </Routes>
+            </Router>
         </div>
     );
 };
