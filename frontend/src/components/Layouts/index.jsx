037b2c5224960353d781bf9c8d6218d658313e95
Hoàng Bình <hpbinh200x@gmail.com>
2025-06-27 15:36:05 +0700

    giao diện frontend

--- frontend/src/components/Layouts/index.jsx
+++ frontend/src/components/Layouts/index.jsx
@@ -1,3 +1,3 @@
-export { default as Header } from './DefaultLayout/Header';
+export { default as Header } from './DefaultLayout/customers/Header';
 export { default as Content } from './Content/Content';
-export { default as Footer } from './DefaultLayout/Footer';
+export { default as Footer } from './DefaultLayout/customers/Footer';
