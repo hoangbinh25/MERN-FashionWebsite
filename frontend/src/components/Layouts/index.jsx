b1278e96312092b6f0e0314a1dc6610a90073535
Hoàng Bình <hpbinh200x@gmail.com>
2025-07-02 01:35:03 +0700

    again

--- frontend/src/components/Layouts/index.jsx
+++ frontend/src/components/Layouts/index.jsx
@@ -1,3 +1,3 @@
-export { default as DefaultLayout } from './DefaultLayout/Customers';
+export { default as DefaultLayout } from './DefaultLayout/Customers/index';
 export { default as Footer } from './DefaultLayout/Customers/Footer';
 export { default as Header } from './DefaultLayout/Customers/Header';
