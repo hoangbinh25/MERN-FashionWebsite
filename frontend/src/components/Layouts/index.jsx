22ac5b8c8a85a613fa2b32819f8260184d8e5ced
Hoàng Bình <hpbinh200x@gmail.com>
2025-07-02 02:01:56 +0700

    test 2

--- frontend/src/components/Layouts/index.jsx
+++ frontend/src/components/Layouts/index.jsx
@@ -1,3 +1,3 @@
-export { default as DefaultLayout } from './DefaultLayout/Customers/index'; //test
-export { default as Footer } from './DefaultLayout/Customers/Footer';
-export { default as Header } from './DefaultLayout/Customers/Header';
+export { default as DefaultLayout } from './DefaultLayout/customers/index'; //test
+export { default as Footer } from './DefaultLayout/customers/Footer';
+export { default as Header } from './DefaultLayout/customers/Header';
