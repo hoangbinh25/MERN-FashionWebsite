b616d0773cb2be67c50751b93e135658c4b74231
Hoàng Bình <hpbinh200x@gmail.com>
2025-07-02 00:43:47 +0700

    conflict

--- frontend/src/routes/index.jsx
+++ frontend/src/routes/index.jsx
@@ -18,26 +18,6 @@ const routes = [
     { path: '/login', component: Login },
     { path: '/register', component: Register },
 
-import HomePage from '~/pages/Customers/HomePage';
-import Shop from '~/pages/Customers/Shop';
-import Blog from '~/pages/Customers/Blog';
-import About from '~/pages/Customers/About';
-import Contact from '~/pages/Customers/Contact';
-
-const routes = [
-    // customers
-    { path: '/', element: <HomePage />, label: 'Home' },
-    { path: '/shop', element: <Shop />, label: 'Shop' },
-    { path: '/blog', element: <Blog />, label: 'Blog' },
-    { path: '/about', element: <About />, label: 'About' },
-    { path: '/contact', element: <Contact />, label: 'Contact' },
-
-    // auth
-
-
-
-    // admin
-
 ]
 
 export default routes;
